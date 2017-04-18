eeb0b166c2fc14ca54145ac4475791ffefa70a02
Trevor OConnor <trevoroc22@gmail.com>
2017-04-18 14:21:27 -0700

    Add update action

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -27,9 +27,19 @@ class CatsController < ApplicationController
     end
   end
 
-  # def edit
-  #
-  # end
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat.id)
+    else
+      render :edit
+    end
+  end
 
   private
 
