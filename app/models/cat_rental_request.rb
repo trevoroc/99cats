cbdbd3ce6d68667f49aeaf9dca4a02d49543e38d
Trevor OConnor <trevoroc22@gmail.com>
2017-04-18 17:21:19 -0700

    Validate request overlap

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,6 +1,28 @@
 class CatRentalRequest < ActiveRecord::Base
   validates :cat_id, :start_date, :end_date, presence: true
   validates :status, inclusion: { in: %w(PENDING APPROVED DENIED) }
+  validate :does_not_overlap_approved_request
 
   belongs_to :cat
+
+  def does_not_overlap_approved_request
+    unless overlapping_approved_requests.empty?
+      errors[:base] << "Cannot overlap with approved rental request"
+    end
+  end
+
+  private
+
+  def overlapping_requests
+    CatRentalRequest
+      .where("((?) > start_date AND (?) < end_date OR (?) > start_date AND (?) < end_date) AND cat_id = (?)",
+            self.start_date, self.start_date, self.end_date, self.end_date, self.cat_id
+            )
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.select do |request|
+      request.status == "APPROVED"
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,30 @@ cat3 = Cat.create(
         sex: 'F',
         description: 'A smart loud cat'
 )
+
+CatRentalRequest.destroy_all
+
+request1 = CatRentalRequest.create(
+            cat_id: cat1.id,
+            start_date: "2017/04/18",
+            end_date: "2017/04/28",
+            status: "APPROVED"
+)
+
+request2 = CatRentalRequest.create(
+            cat_id: cat1.id,
+            start_date: "2017/04/23",
+            end_date: "2017/05/02"
+)
+
+request3 = CatRentalRequest.create(
+            cat_id: cat1.id,
+            start_date: "2017/05/10",
+            end_date: "2017/05/28"
+)
+
+request4 = CatRentalRequest.create(
+            cat_id: cat3.id,
+            start_date: "2017/04/18",
+            end_date: "2017/04/28"
+)
